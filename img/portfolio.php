e4d61d27493a34d6b31dac125101ee3f7ed08f7c
Kyle Raney <raney24fcb@gmail.com>
2018-02-05 03:21:21 -0500

    pages2

---
+++ img/portfolio.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]><html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if (IE 7)&!(IEMobile)]><html class="no-js lt-ie9 lt-ie8" lang="en"><![endif]-->
+<!--[if (IE 8)&!(IEMobile)]><html class="no-js lt-ie9" lang="en"><![endif]-->
+<!--[if (IE 9)]><html class="no-js ie9" lang="en"><![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en-US"> <!--<![endif]-->
+<head>
+
+<!-- Meta Tags -->
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>Portfolio | Cherub Fund</title>   
+
+<meta name="description" content="" /> 
+
+<!-- Mobile Specifics -->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="HandheldFriendly" content="true"/>
+<meta name="MobileOptimized" content="320"/>   
+
+<!-- Mobile Internet Explorer ClearType Technology -->
+<!--[if IEMobile]>  <meta http-equiv="cleartype" content="on">  <![endif]-->
+
+<!-- Bootstrap -->
+<link href="_include/css/bootstrap.min.css" rel="stylesheet">
+
+<!-- Revolution Slider -->
+<link href="_include/rs-plugin/css/settings.css" rel="stylesheet">
+
+<!-- Main Style -->
+<link href="_include/css/main.css" rel="stylesheet">
+
+<!-- FancyBox -->
+<link href="_include/css/fancybox/jquery.fancybox.css" rel="stylesheet">
+
+<!-- Font Icons -->
+<link href="_include/css/fonts.css" rel="stylesheet">
+
+<!-- Shortcodes -->
+<link href="_include/css/shortcodes.css" rel="stylesheet">
+
+<!-- Responsive -->
+<link href="_include/css/responsive.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="_include/css/custom.css" rel="stylesheet">
+
+<!-- Animation CSS -->
+<link href="_include/css/animations.css" rel="stylesheet">
+
+<!-- Google Font -->
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+
+<!-- Fav Icon -->
+<link rel="shortcut icon" href="#">
+
+<link rel="apple-touch-icon" href="#">
+<link rel="apple-touch-icon" sizes="114x114" href="#">
+<link rel="apple-touch-icon" sizes="72x72" href="#">
+<link rel="apple-touch-icon" sizes="144x144" href="#">
+
+<!-- Modernizr -->
+<script src="_include/js/modernizr.js"></script>
+
+<!-- Analytics -->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-4910263-10']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+<!-- End Analytics -->
+
+</head>
+
+
+<body>
+
+<!-- Header -->
+<?php include '_include/php/header.php'; ?>
+<!-- End Header -->
+
+<!-- Start Section Title -->
+<section id="title-page">
+	<h2>Our Portfolio</h2>
+    <span class="arrow"></span>
+</section>
+<!-- End Section Ttile -->
+<br></br>
+
+<div class="container">
+<div style="margin-left:30px">
+<div class="row margin-30">
+            <div class="span12">
+                <h3>2013</h3>
+                
+                <div class="info-block">
+                    <div class="info-text">
+                        <h3><span class="color-text"><a href="http://www.controlmyadhd.com/" target="_blank" class="color-text">Control My ADHD</a></span></h3>
+                        <img src = "_include/img/portfolio/CMADHD.png"><br/>
+			<p>Every week you get a new training module, with a short entertaining video teaching you to deal with common ADHD problem areas, and a bunch of tools and exercises to help you do it! Say goodbye to the headaches and daily frustrations of ADHD, and hello to peaceful and focused mind.</p>
+                        <a class="button-main button-large" target="_blank" href="http://www.controlmyadhd.com/">Website</a> 
+                    </div>
+                </div>
+
+		<div class="info-block">
+                    <div class="info-text">
+			<h3><span class="color-text"><a href="http://www.alumnifyapp.com/" target="_blank" class="color-text">Alumnify</a></span></h3>
+                        <img src = "_include/img/portfolio/Alumnify.png"><br/>
+			<p>Alumnify is an alumni engagement platform that helps organizations keep their alumni involved.</p>
+                        <a class="button-main button-large" target="_blank" href="http://www.alumnifyapp.com/">Website</a> 
+                    </div>
+                </div>
+
+
+		<div class="info-block">
+                    <div class="info-text">
+			<h3><span class="color-text"><a href="http://www.customcollegerecruiting.com/" target="_blank" class="color-text">Customer College Recruiting</a></span></h3>
+                        <img src = "_include/img/portfolio/CCR.png"><br/>
+			<p>Custom College Recruiting (CCR) helps 6,000,000 worldwide high school basketball players to get exposure to over 6,000 college coaches. At the same time, it helps the college coaches to find the right talent among this vast amount of basketball players. This is achieved by CCR's unique method for evaluating high school basketball players.</p>
+                        <a class="button-main button-large" target="_blank" href="http://www.customcollegerecruiting.com/">Website</a> 
+                    </div>
+                </div>            
+	</div>  
+</div>
+</div>
+</div>
+
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+
+<!-- Start Footer -->
+<footer>
+	<div class="container">
+    	<div class="row">
+        	<nav id="social-footer">
+                <ul>
+                    
+
+                </ul>
+            </nav>
+        </div>
+    </div>
+</footer>
+<!-- End Footer -->
+
+<!-- Start Credits -->
+<?php include '_include/php/credit.php'; ?>
+<!-- End Credits -->
+
+<!-- Back To Top -->
+<a id="back-to-top" href="#">
+	<i class="font-icon-arrow-simple-up"></i>
+</a>
+<!-- End Back to Top -->
+
+<!-- Js -->
+<?php include '_include/php/js.php'; ?>
+<!-- End Js -->
+
+</body>
+</html>
